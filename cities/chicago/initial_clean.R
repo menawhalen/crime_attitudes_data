880f12a36970b05bb18b1ad811c551aeec279e7d
Mena Whalen <menawhalen@gmail.com>
2025-05-27 15:35:18 -0500

    adding chicago

---
+++ cities/chicago/initial_clean.R
@@ -0,0 +1,82 @@
+## load packages
+library(tidyverse)
+library(sf)
+library(janitor)
+library(viridis)
+
+############### load data
+
+dat <- read_csv("cities/chicago/old_data/Crimes_-_2001_to_Present_20250527.csv") %>% 
+  clean_names()
+######################################################
+############# cleaning data
+clean_dat <- dat %>%
+  select(date, primary_type, latitude, longitude) %>% ## usinged cmplt_fr_dt as date since is the earliest crime date and the most accurate time of the incident
+  mutate(date = mdy_hms(date), # change to date
+         ofns_desc = str_to_lower(primary_type)) %>%  # make to lower case
+  select(-c(primary_type)) %>%
+  filter(date > ymd_hms("2015-12-31 23:59:59")) %>% 
+  filter(!is.na(latitude) & !is.na(longitude)) %>% ## cant use if missing
+  mutate(desc_crime_type = case_when(
+    str_detect(ofns_desc, "murder|manslaughter|homicide") ~ "homicide",
+    str_detect(ofns_desc, "motor vehicle theft") ~ "mvt",
+    str_detect(ofns_desc, "sex|rape") ~ "sex crimes",
+    str_detect(ofns_desc, "robbery") ~ "robbery",
+    str_detect(ofns_desc, "assault") ~ "assault",
+    str_detect(ofns_desc, "larceny|theft") ~ "theft",
+    str_detect(ofns_desc, "burglary") ~ "burglary",
+    str_detect(ofns_desc, "arson") ~ "arson",
+    #str_detect(ofns_desc, "harrassment") ~ "harrassment",
+    str_detect(ofns_desc, "weapon") ~ "weapon",
+    str_detect(ofns_desc, "drug|narcotic") ~ "drugs",
+    .default = "other"
+  ))
+## loosing about 2,000
+
+
+############# zip codes
+### zip code joins
+zips <- read_rds("us_zctas_2016_2023.rds")
+## this has all the same goemetrys from 2021 onward to 2023
+
+joined_zip <- st_join(st_as_sf(clean_dat, coords = c("longitude", "latitude"), crs = st_crs(zips)),
+                      select(zips, year, geoid, estimate))
+
+zip_crime <- joined_zip %>% 
+  tibble() %>% 
+  mutate(year_crime = ifelse(year(date) > 2023, 2023, year(date))) %>% ## assumes pop after 2023 is same as 2023 and actually comes from same 2021 shapefiles in larger dataset
+  filter(year == year_crime) %>% 
+  select(date, desc_crime_type, geoid, estimate)
+
+
+final <- zip_crime %>% 
+  rename(zcta = geoid, population = estimate) %>% 
+  mutate(month = month(date, label = T, abbr = T),
+         year = year(date),
+         city = "chicago") %>% 
+  group_by(city, month, year, zcta, desc_crime_type, population) %>% 
+  tally(name = "count") %>% 
+  mutate(rate = count/population*100000)
+
+write_csv(final, "cities/chicago/current.csv")
+write_csv(dat, "cities/chicago/full_uncleaned_current.csv")
+
+clean_dat %>% 
+  mutate(year = year(date)) %>% 
+  ggplot(aes(factor(year))) +
+  geom_bar(stat = "count")
+
+tibble(final) %>% 
+  mutate(zcta = as.numeric(zcta)) %>% 
+  group_by(year, zcta) %>% 
+  reframe(total = sum(count)) %>% 
+  pivot_wider(id_cols = zcta, names_from = year, values_from = total) %>% view()
+
+summary(clean_dat$date)
+
+final %>% 
+  group_by(year, desc_crime_type) %>% 
+  summarise(total = sum(count)) %>% 
+  ggplot(aes(factor(year), total, group = desc_crime_type)) +
+  geom_line() +
+  facet_wrap(~desc_crime_type, scales = "free_y")
